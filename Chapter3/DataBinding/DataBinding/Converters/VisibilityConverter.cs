c79eee8fb016c6df891c795643bea881214b5df2
jeremylikness_cp <SND\jeremylikness_cp>
2012-06-05 10:41:29 +0000

    Initial check-in for Release Preview

---
+++ Chapter3/DataBinding/DataBinding/Converters/VisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace DataBinding.Converters
+{
+    public class VisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null || !(value is bool))
+                return Visibility.Visible;
+
+            var parm = parameter ?? false;
+            bool flip;
+            Boolean.TryParse(parm.ToString(), out flip);
+            var visible = flip ? (bool)value : !((bool)value);
+            return visible ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
