9a90cfc4cedc395909923b1653dfb57a6b1a54d1
Jeremy Likness <jeremy@jeremylikness.com>
2012-06-05 10:49:38 +0000

    Fix path

---
+++ Chapter3/DependencyProperties/DependencyProperties/MyDependencyObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using Windows.UI.Xaml;
+
+namespace DependencyProperties
+{
+    public class MyDependencyObject : DependencyObject 
+    {
+        public static readonly DependencyProperty
+            MyNumberProperty = DependencyProperty.Register(
+            "MyNumber", typeof(int), typeof(MyDependencyObject), 
+            new PropertyMetadata(2, PropertyChange));
+
+        private static void PropertyChange(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            Debug.WriteLine("Property changed: {0}", e.NewValue);
+        }
+
+        public int MyNumber
+        {
+            get { return (int)GetValue(MyNumberProperty); }
+            set { SetValue(MyNumberProperty, value); }
+        }
+    }
+
+
+}
