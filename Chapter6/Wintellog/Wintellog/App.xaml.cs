26ebbe65e2133eaff7ab8f47c46ab64e9a3ce83f
Jeremy Likness <jeremy@jeremylikness.com>
2012-06-23 15:52:53 +0000

    Initial check-in for Release Preview

---
+++ Chapter6/Wintellog/Wintellog/App.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Wintellog.Common;
+using Wintellog.DataModel;
+
+// The Grid App template is documented at http://go.microsoft.com/fwlink/?LinkId=234226
+
+namespace Wintellog
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App
+    {
+        /// <summary>
+        /// Initializes the singleton Application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            InitializeComponent();
+            Suspending += OnSuspending;
+        }
+
+        public static App Instance
+        {
+            get { return ((App) Current); }
+        }
+
+        public BlogDataSource DataSource { get; private set; }
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used when the application is launched to open a specific file, to display
+        /// search results, and so forth.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            // Do not repeat app initialization when already running, just ensure that
+            // the window is active
+            if (args.PreviousExecutionState == ApplicationExecutionState.Running)
+            {
+                Window.Current.Activate();
+                return;
+            }
+
+            DataSource = new BlogDataSource();
+
+            var splashScreen = args.SplashScreen;
+            var splash = new SplashPage(splashScreen, args);
+            splashScreen.Dismissed += splash.DismissedEventHandler;
+            Window.Current.Content = splash;
+            Window.Current.Activate();            
+        }
+
+        /// <summary>
+        /// Invoked when application execution is being suspended.  Application state is saved
+        /// without knowing whether the application will be terminated or resumed with the contents
+        /// of memory still intact.
+        /// </summary>
+        /// <param name="sender">The source of the suspend request.</param>
+        /// <param name="e">Details about the suspend request.</param>
+        private async void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            await SuspensionManager.SaveAsync();
+            deferral.Complete();
+        }
+    }
+}
