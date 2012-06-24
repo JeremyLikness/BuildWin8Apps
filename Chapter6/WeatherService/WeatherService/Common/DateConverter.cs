e493bc24e5e876c0b0c3fa6c87152c1831f13543
Jeremy Likness <jeremy@jeremylikness.com>
2012-06-24 16:32:20 +0000

    Initial check-in for WeatherService sample application (Chapter 6)

---
+++ Chapter6/WeatherService/WeatherService/Common/DateConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace WeatherService.Common
+{
+    public class DateConverter : IValueConverter 
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return ((DateTime) value).ToString("D");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
