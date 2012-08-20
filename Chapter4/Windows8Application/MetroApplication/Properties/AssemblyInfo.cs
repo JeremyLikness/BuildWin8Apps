2bded63f046d30fb279bc9e18c809b82a6de6b50
jeremylikness_cp <SND\jeremylikness_cp>
2012-08-20 22:51:48 +0000

    further fix-up for Metro references (Chapter 4)

--- Chapter4/Windows8Application/MetroApplication/Properties/AssemblyInfo.cs
+++ Chapter4/Windows8Application/MetroApplication/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MetroApplication")]
+[assembly: AssemblyTitle("Windows 8 Application")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MetroApplication")]
+[assembly: AssemblyProduct("Windows8Application")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
